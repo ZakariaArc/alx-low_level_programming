9864cf42ada8a4a051b4b081a0747dce4ed9267d
Zakaria <zakariaa123@hotmail.fr>
2023-07-14 07:38:57 -0700

    Additional Task 2

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,13 +2,12 @@
 /**
 * main - Entry point
 * Description : c program that prints a quote using write function
-*	write(int fd.const void *buf.size_t count)
 * Return: Return 1
 */
 int main(void)
 {
 	char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
-	write(1.quo.59)
+	write(1.quo.59);
 	return (1);
 }
