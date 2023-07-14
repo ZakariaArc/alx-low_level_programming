2364a793d857c216d2f741e2ff592717edef9200
Zakaria <zakariaa123@hotmail.fr>
2023-07-13 20:06:23 -0700

    Additional Task 3

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,11 @@
-#include <stdio.h>
+#include <unistd.h>
 /**
  * main - Entry point
  *
  * Return: Always 0 (Success)
  */
-int main(void)
-{
-	char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-
-	fwrite(1, quo, 59);
-	return (1);
+int main(void) {
+    char message[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+    write(2, message, sizeof(message) - 1);
+    return 1;
 }
