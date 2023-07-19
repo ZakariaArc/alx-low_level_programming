bc7997ca1d3a4e69a7b19a17cb2cc115b18fcfed
Zakaria <zakariaa123@hotmail.fr>
2023-07-18 19:40:26 -0700

    Task 5

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_sign - prints the sign of the number
+ * @n: the int to check
+ * Return: if + return 1, if 0 return 0, if - return -1
+*/
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	} else if (n == 0)
+	{
+		_putchar(48);
+		return (0);
+	} else if (n < 0)
+	{
+		_putchar('-');
+	}
+		return (-1);
+}
