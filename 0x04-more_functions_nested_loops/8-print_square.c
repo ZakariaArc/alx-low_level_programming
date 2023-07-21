9c92748a5bb78ff356643d272eba3ae69c89d2eb
Zakaria <zakariaa123@hotmail.fr>
2023-07-20 19:41:16 -0700

    Task 8

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -1,1 +1,17 @@
+#include"main.h"
+/**
+ * print_square - print a square
+ * @size: size of the square
+ * Return: 0 (success)
+*/
+void print_square(int size)
+{
+	int row, column;
 
+	for (row = 1; row <= size; ++row)
+	{
+		for (column = 1; column <= size; ++column)
+			_putchar('#');
+		_putchar('\n');
+	}
+}
