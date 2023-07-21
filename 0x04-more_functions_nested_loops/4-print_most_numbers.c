463806e47bd76f2be83e2f339be77b23434f3942
Zakaria <zakariaa123@hotmail.fr>
2023-07-20 19:29:15 -0700

    Task 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,17 @@
+#include"main.h"
+/**
+ * print_most_numbers - prints from 0 to 9
+ * Return: 0 (success)
+*/
+void print_most_numbers(void)
+{
+	int num;
+
+	for (num = 0; num <= 9; ++num)
+	{
+		if (num == 2 || num == 4)
+			continue;
+		_putchar(num + 48);
+	}
+	_putchar('\n');
+}
