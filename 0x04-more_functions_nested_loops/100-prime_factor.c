1752f0822dd55bd43b571c3814312b1d876d39a0
Zakaria <zakariaa123@hotmail.fr>
2023-07-20 19:55:33 -0700

    Task 100

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,50 +1,36 @@
 #include <stdio.h>
 /**
- * custom_sqrt - custom square root function to find integer square root
- * @n: input number
- * Return: integer square root of n
- */
-long int custom_sqrt(long int n) {
-    long int x = n;
-    long int y = (x + 1) / 2;
-
-    while (y < x) {
-        x = y;
-        y = (x + n / x) / 2;
-    }
-
-    return x;
-}
-
-/**
- * main - finds and prints the largest prime factors of 612852475143
+ * main - finds and prints the largest prime factor of 612852475143
  * Return: 0 (success)
  */
-int main(void) {
-    long int n;
-    long int max;
-    long int i;
+int main(void)
+{
+	long int n;
+	long int max;
+	long int i;
 
-    n = 612852475143;
-    max = -1;
+	n = 612852475143;
+	max = -1;
 
-    while (n % 2 == 0) {
-        max = 2;
-        n /= 2;
-    }
+	while (n % 2 == 0)
+	{
+		max = 2;
+		n /= 2;
+	}
 
-    for (i = 3; i <= custom_sqrt(n); i = i + 2) {
-        while (n % i == 0) {
-            max = i;
-            n = n / i;
-        }
-    }
+	for (i = 3; i <= sqrt(n); i = i + 2)
+	{
+		while (n % i == 0)
+		{
+			max = i;
+			n = n / i;
+		}
+	}
 
-    if (n > 2)
-        max = n;
+	if (n > 2)
+		max = n;
 
-    printf("%ld\n", max);
+	printf("%ld\n", max);
 
-    return 0;
+	return (0);
 }
-
