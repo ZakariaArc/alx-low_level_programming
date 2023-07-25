d229ea667e2b807476643242b12029f2494f8c21
Zakaria <zakariaa123@hotmail.fr>
2023-07-24 18:46:47 -0700

    Task 0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+  * reset_to_98 - takes a poinyer to an int as
+  * parameter and updates the value to points to to 98
+  *
+  * @n: the int parameter
+  * 
+  * Return: nothing
+  */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
