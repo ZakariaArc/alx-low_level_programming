7b89139a640e114544ee46230220239a50814a5f
Zakaria <zakariaa123@hotmail.fr>
2023-07-24 19:22:32 -0700

    Task 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+  * print_rev - prints a string in reverse
+  *@s: the input string
+  * Return: nothing
+  */
+
+void print_rev(char *s)
+{
+	if (s == NULL)
+		return;
+
+	int length = 0;
+
+	while (s[length] != '\0')
+		length++;
+
+	for (int i = length - 1; i >= 0; i--)
+		-putchar(s[i]);
+
+	_putchar('\n');
+}
