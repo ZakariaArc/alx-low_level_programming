fcd44d6c94cf41a1cc3e1e620c334577ea397290
Zakaria <zakariaa123@hotmail.fr>
2023-07-24 19:31:38 -0700

    Changed the code for Task 4

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,23 +1,17 @@
 #include "main.h"
 /**
   * print_rev - prints a string in reverse
-  *@s: the input string
+  * @s: the input string
   * Return: nothing
   */
 
 void print_rev(char *s)
 {
-	if (s == NULL)
-		return;
+	int alpha;
 
-	int length = 0;
-	int i;
-
-	while (s[length] != '\0')
-		length++;
-
-	for (i = length - 1; i >= 0; i--)
-		-putchar(s[i]);
-
+	for (alpha = 0; s[alpha] != '\0'; ++alpha)
+		;
+	for (--alpha; alpha >= 0; --alpha)
+		_putchar(s[alpha]);
 	_putchar('\n');
 }
