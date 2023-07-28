79518826477e9e751d0448221beed95af6502b4e
Zakaria <zakariaa123@hotmail.fr>
2023-07-27 17:31:22 -0700

    Task 101

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -10,30 +10,16 @@ void print_number(int n)
 
 	if (n < 0)
 	{
-		_putchar('-');
 		n1 = -n;
+		_putchar('-');
 	}
 	else
 	{
 		n1 = n;
 	}
 
-	unsigned int divisor = 1;
-	unsigned int temp = n1;
+	if (n1 / 10)
+		print_number(n1 / 10);
 
-	while (temp / 10 != 0)
-	{
-		divisor *= 10;
-		temp /= 10;
-	}
-
-	while (divisor != 0)
-	{
-		int digit = n1 / divisor;
-
-		_putchar(digit + '0');
-		n1 -= (digit * divisor);
-		divisor /= 10;
-	}
+	_putchar((n1 % 10) + '0');
 }
-
