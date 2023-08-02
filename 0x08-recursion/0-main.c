ebd76c423d74b1c12d96d8396122054a5d1af002
Zakaria <zakariaa123@hotmail.fr>
2023-08-02 15:30:17 -0700

    Task 0

--- 0x08-recursion/0-main.c
+++ 0x08-recursion/0-main.c
@@ -1,17 +1,12 @@
 #include "main.h"
-/**
- * _puts_recursion - a function that prints a new line
- * @s: the string to print
- * Return: nothing (success)
-*/
 
-void _puts_recursion(char *s)
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
 {
-	if (*s == '\0')
-	{
-		putchar('\n');
-		return;
-	}
-	putchar(*s);
-	_puts_recursion(s + 1);
+	_puts_recursion("Puts with recursion");
+	return (0);
 }
--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stdio.h>
 /**
  * _puts_recursion - a function that prints a new line
  * @s: the string to print
@@ -10,7 +9,7 @@ void _puts_recursion(char *s)
 {
 	if (*s == '\0')
 	{
-		putchar('\n');
+		_putchar('\n');
 		return;
 	}
 	_putchar(*s);
