ee98f617f77a7581d019a4b1c82ce61bf43b0c3e
Zakaria <zakariaa123@hotmail.fr>
2023-08-16 03:20:43 -0700

    Task 0

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,17 @@
+#include "function_pointers.h"
+#include <stdio.h>
+/**
+ * print_name - function that prints a name using a specified function
+ * @name: name to be printed
+ * @f: a pointer to the function used to print the name
+ * Return: nothing
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	if (name != NULL && f != NULL)
+	{
+		f(name);
+		printf("\n");
+	}
+}
--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,5 +1,6 @@
 #ifndef _FUNCTION
 #define _FUNCTION
+#include <stddef.h>
 
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
