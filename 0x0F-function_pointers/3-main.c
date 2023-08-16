93366b8427c97d56753476eb386f8186f94dd2d3
Zakaria <zakariaa123@hotmail.fr>
2023-08-16 05:55:55 -0700

    Task 3

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,43 @@
+#include "3-calc.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - the entry point for the calculator program
+ * @argc: the number of arguments
+ * @argv: the array of arguments
+ * Return: 0 (Success)
+ */
+ 
+int main(int argc, char *argv[])
+{
+	int num1, num2;
+	int result;
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		return (98);
+	}
+
+    num1 = atoi(argv[1]);
+    num2 = atoi(argv[3]);
+
+	int (*op_func)(int, int) = get_op_func(argv[2]);
+
+	if (!op_func)
+	{
+		printf("Error\n");
+		return (99);
+    }
+
+	if ((*argv[2] == '/' || *argv[2] == '%') && num2 == 0)
+	{
+		printf("Error\n");
+		return (-100);
+    }
+
+	result = op_func(num1, num2);
+	printf("%d\n", result);
+
+	return (0);
+}
