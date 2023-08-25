e4ec5c65cb403d661b3a80df9845bd920df06de9
Zakaria <zakariaa123@hotmail.fr>
2023-08-24 17:41:34 -0700

    Task 100

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
+/**
+  * before_main - function that runs before main
+  * Description: this function is a constructor that runs before the main
+  *              function and prints a specific message
+  */
 
 void __attribute__((constructor)) before_main()
 {
-	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n"
+			"I bore my house upon my back!\n");
 }
