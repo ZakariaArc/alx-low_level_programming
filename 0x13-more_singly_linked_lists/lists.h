acd8a522fea5d1cf8ee79b8f5ac954dd99bfc0bf
Zakaria <zakariaa123@hotmail.fr>
2023-08-29 11:47:26 -0700

    Task 0

---
+++ 0x13-more_singly_linked_lists/lists.h
@@ -0,0 +1,19 @@
+#ifndef _LISTS
+#define _LISTS
+#include <stddef.h>
+/**
+ * struct listint_s - the singly linked list
+ * @n: an integer
+ * @next: the points to the next node
+ * Description: the singly linked list node structure
+ */
+
+typedef struct listint_s
+{
+	int n;
+	struct listint_s *next;
+} listint_t;
+
+size_t print_listint(const listint_t *h);
+
+#endif
