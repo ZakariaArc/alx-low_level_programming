11cc141648b70a28069a9f0d61eca7188be2341d
Zakaria <zakariaa123@hotmail.fr>
2023-09-04 16:51:39 -0700

    Task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -8,23 +8,23 @@
 
 int create_file(const char *filename, char *text_content)
 {
-    int fd, write_status;
-    mode_t mode = S_IRUSR | S_IWUSR;
+	int fd, write_status;
+	mode_t mode = S_IRUSR | S_IWUSR;
 
-    if (filename == NULL)
-        return (-1);
-    fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, mode);
-    if (fd == -1)
-        return (-1);
-    if (text_content != NULL)
-    {
-        write_status = write(fd, text_content, strlen(text_content));
-        if (write_status == -1)
-        {
-            close(fd);
-            return (-1);
-        }
-    }
-    close(fd);
-    return (1);
-}
+	if (filename == NULL)
+		return (-1);
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, mode);
+	if (fd == -1)
+		return (-1);
+	if (text_content != NULL)
+	{
+		write_status = write(fd, text_content, strlen(text_content));
+		if (write_status == -1)
+		{
+			close(fd);
+			return (-1);
+		}
+	}
+	close(fd);
+	return (1);
+}²
