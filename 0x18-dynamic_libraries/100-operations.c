715264f619365d0521dabe7de736cdbe397981fd
Zakaria <zakariaa123@hotmail.fr>
2023-10-16 16:05:56 -0700

    Task 2

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,60 @@
+#include "100-operations.h"
+/**
+ * add - adds two integers
+ * @a: the first integer
+ * @b: the second integer
+ * Return: the sum of a and b
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * sub - subtracts two integers
+ * @a: the first integer
+ * @b: the second integer
+ * Return: the difference of a and b
+ */
+
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * mul - multiplies two integers
+ * @a: the first integer
+ * @b: the second integer
+ * Return: the product of a and b
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * div - divides two integers
+ * @a: the first integer
+ * @b: the second integer
+ * Return: the result of the division of a by b
+ */
+
+int div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * mod - computes the remainder of the division of two integers
+ * @a: the first integer
+ * @b: the second integer
+ * Return: the remainder of the division of a by b
+ */
+
+int mod(int a, int b)
+{
+	return (a % b);
+}
---
+++ 0x18-dynamic_libraries/100-operations.h
@@ -0,0 +1,10 @@
+#ifndef _100_OPERATIONS
+#define _100_OPERATIONS
+
+int add(int a, int b);
+int sub(int a, int b);
+int mul(int a, int b);
+int div(int a, int b);
+int mod(int a, int b);
+
+#endif
