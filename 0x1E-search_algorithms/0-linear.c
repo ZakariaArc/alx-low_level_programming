45400cf01a12e9cc5399c9041850198adc01369d
ZakariaArc <zakariaa123@hotmail.fr>
2024-02-07 15:04:34 -0800

    Task 0

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -15,16 +15,25 @@
 
 int linear_search(int *array, size_t size, int value)
 {
-	int i;
+	size_t i;
 
+	/* Check if the array is NULL */
 	if (array == NULL)
 		return (-1);
 
-	for (i = 0; i < (int)size; i++)
+	/* Iterate over each element of the array */
+	for (i = 0; i < size; i++)
 	{
-		printf("Value checked array[%u] = [%d]\n", i, array[i]);
-		if (value == array[i])
+		/* Print the value being checked */
+		printf("Value checked array[%ld] = [%d]\n", i, array[i]);
+
+		/* If the value is found */
+		if (array[i] == value)
+
+			/* Return the index of the value */
 			return (i);
 	}
+
+	/* Value not found, return -1 */
 	return (-1);
 }
